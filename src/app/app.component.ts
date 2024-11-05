55c002787006df58f66fac43f2b3fca3f840668f
fabricionetto <agostinhonetto73@gmail.com>
2024-11-05 19:38:20 -0300

    estudo-front-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-user',
+  template:`Username: {{ username }}`,
+  standalone: true
+})
+export class UserComponent {
+  username = 'oPA LELELE';;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `<app-user />`,
+  imports: [UserComponent],
 })
-export class AppComponent {
-  title = 'crud-ang';
-}
+export class AppComponent {}
