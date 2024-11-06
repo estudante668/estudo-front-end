b3aed264a8ef17c814c43c8d46d8bebf99c53aa3
fabricionetto <agostinhonetto73@gmail.com>
2024-11-05 21:51:20 -0300

    @if-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
-
-@Component({
-  selector: 'app-user',
-  template:`Username: {{ username }}`,
-  standalone: true
-})
-export class UserComponent {
-  username = 'Composição de componentes';;
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<app-user />`,
-  imports: [UserComponent],
+  templateUrl: `app.component.html`,
+  styleUrl:`app.component.scss`,
+  imports: [RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent {
+  isLoggin = true;
+}
